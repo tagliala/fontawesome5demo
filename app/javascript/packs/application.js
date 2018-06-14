3ef4a363a9ca717fa03946daa71ac7892438eb77
Geremia Taglialatela <tagliala.dev@gmail.com>
2018-06-14 15:48:36 +0200

    Add Font Awesome JavaScript

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,4 +7,26 @@
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 
-console.log('Hello World from Webpacker')
+import { library, dom } from '@fortawesome/fontawesome-svg-core'
+
+import {
+  faFontAwesome
+} from '@fortawesome/free-brands-svg-icons'
+
+import {
+  faStar as farFaStar
+} from '@fortawesome/free-regular-svg-icons';
+
+import {
+  faStar as fasFaStar
+} from '@fortawesome/free-solid-svg-icons';
+
+library.add(
+  faFontAwesome,
+  farFaStar,
+  fasFaStar
+);
+
+// Change `observeMutationsRoot` value to `document.documentElement`
+// to ensure compatibility with Turbolinks
+dom.watch({ observeMutationsRoot: document.documentElement })
